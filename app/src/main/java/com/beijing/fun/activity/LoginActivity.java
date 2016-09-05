c974261588319a791e15b432489d73807c23d47e
孟祥程 <csdn@vip.163.com>
2016-09-05 16:35:06 +0800

    first commit

---
+++ app/src/main/java/com/beijing/fun/activity/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.beijing.fun.activity;
+
+
+import com.beijing.fun.BaseActivity;
+import com.beijing.fun.R;
+
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * @author CentMeng csdn@vip.163.com on 16/8/11.
+ */
+@EActivity(R.layout.activity_login)
+public class LoginActivity extends BaseActivity{
+}
---
+++ app/src/main/java/com/beijing/fun/activity/MainActivity.java
@@ -0,0 +1,9 @@
+package com.beijing.fun.activity;
+
+import com.beijing.fun.BaseActivity;
+
+/**
+ * @author CentMeng csdn@vip.163.com on 16/8/11.
+ */
+public class MainActivity extends BaseActivity{
+}
